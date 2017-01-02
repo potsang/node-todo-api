4c09dd0814b902b42254e01a3d4cddaf5d0655dc
Potsang Huang <freelancer5858@yahoo.com>
2017-01-01 18:21:52 -0600

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,16 +33,16 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)) {
-      res.status(404).send();
+      return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(404).send();
+      return res.status(404).send();
     }
-    res.send(todo);
+    res.send({todo});
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   });
 });
 
